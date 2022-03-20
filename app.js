344534fd9ae7ced7213ff5ab60d822e98377550d
HarshaVardhanDodla <dodlaharshanani@gmail.com>
2022-03-20 18:51:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const advice = async () =>
+{
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    document.getElementById("title").innerHTML=`advice #${data.slip.id}`
+    document.getElementById("message").innerHTML = `"${data.slip.advice}"`
+}
+
+advice()
